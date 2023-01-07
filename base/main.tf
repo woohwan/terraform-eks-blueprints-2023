1dba46a400f6b83ec36c1e51027a9386871b9c48
woohwan <whpark70@gmail.com>
2023-01-07 17:11:34 +0900

    add aws addon

--- base/main.tf
+++ base/main.tf
@@ -68,6 +68,22 @@ module "eks_blueprints" {
   tags = local.tags
 }
 
+module "eks_blueprints_kubernetes_addons" {
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons?ref=v4.20.0"
+
+  eks_cluster_id       = module.eks_blueprints.eks_cluster_id
+  eks_cluster_endpoint = module.eks_blueprints.eks_cluster_endpoint
+  eks_oidc_provider    = module.eks_blueprints.oidc_provider
+  eks_cluster_version  = module.eks_blueprints.eks_cluster_version
+
+  # EKS Managed Add-ons
+  enable_amazon_eks_vpc_cni            = true
+  enable_amazon_eks_coredns            = true
+  enable_amazon_eks_kube_proxy         = true
+  enable_amazon_eks_aws_ebs_csi_driver = true
+
+  tags = local.tags
+}
 
 #---------------------------------------------------------------
 # Supporting Resources
