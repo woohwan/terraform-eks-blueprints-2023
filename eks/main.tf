32c4b1e1821edd6bed02f5557fe3f2a2e4512333
woohwan <whpark70@naver.com>
2023-02-21 16:16:03 +0900

    max=1

--- eks/main.tf
+++ eks/main.tf
@@ -60,7 +60,7 @@ module "eks_blueprints" {
       instance_types  = ["t3.medium"]
       min_size        = 1
       max_size        = 3
-      desired_size    = 2
+      desired_size    = 1
       subnet_ids      = module.vpc.private_subnets
     }
   }
